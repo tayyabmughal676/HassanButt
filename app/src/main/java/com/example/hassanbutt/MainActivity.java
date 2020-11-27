ddcc0bf9e7bd7007321a6ddb501dff69327a8145
Tayyabmughal676 <Tayyabmughal676@gmail.com>
2020-11-27 15:31:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/hassanbutt/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.hassanbutt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //    Declared
+    private Button mSignUpBtn;
+    private EditText mUserName, mEmail, mPassword, mRetyepPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Set ID Initialized
+        mSignUpBtn = findViewById(R.id.signUpBtn);
+        mUserName = findViewById(R.id.editTextName);
+        mEmail = findViewById(R.id.editTextEmail);
+        mPassword = findViewById(R.id.editTextPassword);
+        mRetyepPassword = findViewById(R.id.editTextRetypePassword);
+
+//        Lambda click
+        mSignUpBtn.setOnClickListener(v -> {
+            userSignUp();
+        });
+
+    }
+
+    private void userSignUp() {
+        String name = mUserName.getText().toString().trim();
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String retypePassword = mRetyepPassword.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            mUserName.setError("Enter User Name");
+        }
+        if (email.isEmpty()) {
+            mEmail.setError("Enter Email");
+        }
+        if (password.isEmpty()) {
+            mPassword.setError("Enter Password");
+        }
+        if (retypePassword.isEmpty()) {
+            mRetyepPassword.setError("Enter Retype Password");
+        }
+
+        Toast.makeText(this,
+                "Name is: " + name +
+                        "Email is: " + email,
+                Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Welcome " + name, Toast.LENGTH_SHORT).show();
+
+    }
+}
